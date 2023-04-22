4f6ec7b3916f79272f707cbe627953635c9dc960
Evgeny <jenkneo.lal@gmail.com>
2023-04-22 13:02:00 +0400

    Базовые модели для рецептов

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,137 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    """Ингридиенты для рецепта"""
+
+    name = models.CharField(
+        verbose_name='Ингридиент',
+        max_length=32,
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единицы измерения',
+        max_length=24,
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """Тэги для рецептов"""
+
+    name = models.CharField(
+        verbose_name='Тэг',
+        max_length=64,
+        unique=True,
+    )
+    color = models.CharField(
+        verbose_name='Цвет',
+        max_length=7,
+        unique=True,
+        db_index=False,
+    )
+    slug = models.CharField(
+        verbose_name='Slug тэга',
+        max_length=32,
+        unique=True,
+        db_index=False,
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    """Модель для рецептов"""
+
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        related_name='recipes',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    name = models.CharField(
+        verbose_name='Название блюда',
+        max_length=64,
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег',
+        related_name='recipes'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиенты блюда',
+        related_name='recipes',
+        through='recipes.AmountIngredient',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        editable=False,
+    )
+    image = models.ImageField(
+        verbose_name='Изображение блюда',
+        upload_to='recipe_images/',
+    )
+    text = models.TextField(
+        verbose_name='Описание блюда',
+        max_length=512,
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        # Время приготовления в минутах
+        verbose_name='Время приготовления',
+        default=0
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date', )
+
+    def __str__(self):
+        return self.name
+
+
+class AmountIngredient(models.Model):
+    """Количество ингридиентов в блюде"""
+
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='В каких рецептах',
+        related_name='ingredient',
+        on_delete=models.CASCADE,
+    )
+    ingredients = models.ForeignKey(
+        Ingredient,
+        verbose_name='Связанные ингредиенты',
+        related_name='recipe',
+        on_delete=models.CASCADE,
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        default=0
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Количество ингридиентов'
+        ordering = ('recipe', )
+
+    def __str__(self) -> str:
+        return f'{self.amount} {self.ingredients}'
