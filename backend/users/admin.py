86d153977147d5e4885a6bdd6ff3a378d54c6bab
Evgeny <jenkneo.lal@gmail.com>
2023-04-22 13:57:48 +0400

    Добавление отображения моделей в админ-панель

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import MyUser
+from .models import MyUser, Subscriptions, Favorites, Carts
 
 admin.site.register(MyUser)
+admin.site.register(Subscriptions)
+admin.site.register(Favorites)
+admin.site.register(Carts)
