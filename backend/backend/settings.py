e64af84aa394acdbb941abd3408777e0ca5eba4b
Evgeny <jenkneo.lal@gmail.com>
2023-04-23 10:32:38 +0400

    Создано и подключено приложение api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
 ]
