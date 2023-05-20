af60a43e9f883ee6ad5ba81df189fd78f6cc1067
Evgeny <jenkneo.lal@gmail.com>
2023-05-21 01:21:41 +0400

    Добавление моделей в админку.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 
 from .models import Ingredient, Tag, Recipe, AmountIngredient
 
-admin.site.register(Ingredient)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'favorite_count')
+    list_editable = (
+        'name', 'cooking_time', 'text', 'tags',
+        'image', 'author'
+    )
+    readonly_fields = ('favorite_count',)
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = '-пусто-'
+
+    def favorite_count(self, obj):
+        return obj.favorite.count()
+
+    favorite_count.short_description = 'Добавлений в избранное'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
 admin.site.register(Tag)
-admin.site.register(Recipe)
 admin.site.register(AmountIngredient)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from .models import MyUser, Subscriptions, Favorites, Carts
 
-admin.site.register(MyUser)
+
+@admin.register(MyUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+         'id', 'username', 'email', 'first_name', 'last_name',
+    )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Subscriptions)
 admin.site.register(Favorites)
 admin.site.register(Carts)
